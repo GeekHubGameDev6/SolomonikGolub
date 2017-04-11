e3b2924153297ff9dbf814387425137d5f5324d8
Yaroslav Golub <neverfo@gmail.com>
2017-04-11 20:53:50 +0300

    MatchPictureGame

---
+++ Assets/Scripts/MatchPictures/DropZone.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    bool main;
+    public void OnDrop(PointerEventData eventData)
+    {
+        Dragable d = eventData.pointerDrag.GetComponent<Dragable>();
+        if (d != null && this.transform.childCount > 0)
+        {
+            d.parentToReturnTo = this.transform;
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag == null || this.transform.childCount > 0)
+            return;
+
+        Dragable d = eventData.pointerDrag.GetComponent<Dragable>();
+        if(d != null)
+        {
+            d.placeHolderParent = this.transform;
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag == null || this.transform.childCount > 0)
+            return;
+
+        Dragable d = eventData.pointerDrag.GetComponent<Dragable>();
+        if(d != null && d.placeHolderParent == this.transform)
+        {
+            d.placeHolderParent = d.parentToReturnTo;
+        }
+    }
+}
---
+++ Assets/Scripts/MatchPictures/MatchController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MatchController : MonoBehaviour
+{
+
+    public Sprite[] mainSprites;
+    public Sprite[] spritesForMatch;
+
+    public Transform upperParent;
+    public Transform holdersParent;
+
+    public Transform lowerParent;
+
+    public GameObject upperImagePrefab;
+    public GameObject holderPrefab;
+
+    public GameObject lowerImagePrefab;
+
+    public GameObject resultPanel;
+    public Text resultText;
+    public Color right, wrong;
+
+    private void Start()
+    {
+        resultPanel.SetActive(false);
+
+    }
+
+    private void CreateObjects(GameObject prefab, int count, Transform parent, string[] initValue)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            GameObject go = GameObject.Instantiate(prefab);
+            go.GetComponent<Text>().text = initValue[i];
+            go.transform.SetParent(parent);
+        }
+    }
+
+    public void OnCheckBtnClick()
+    {
+        for (int i = 0; i < upperParent.childCount; i++)
+        {
+            if ((int)holdersParent.GetChild(i).childCount <= 0 || ((int)upperParent.GetChild(i).GetComponent<MatchAnimalItem>().animalType !=
+                (int)holdersParent.GetChild(i).GetChild(0).GetComponent<MatchFoodItem>().foodType))
+            {
+                resultPanel.SetActive(true);
+                resultText.text = "You have made a mistake, pls try again";
+                resultText.color = wrong;
+                return;
+            }
+        }
+
+        Debug.Log("Win");
+        resultPanel.SetActive(true);
+        resultText.text = "Everything OK";
+        resultText.color = right;
+    }
+
+
+}
+
+public enum AnimalType
+{
+    Fish = 0,
+    Dog,
+    Cat,
+    Bear,
+    Horse
+}
+
+public enum FoodType
+{
+    Worm = 0,
+    Bone,
+    Fish,
+    Honey,
+    Grass
+}
