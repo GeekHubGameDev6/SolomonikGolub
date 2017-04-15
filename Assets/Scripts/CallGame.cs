ec98c3dac34664e0bcb9c87fab9e33d9557ff970
isolomonik <irina.solomonik2@gmail.com>
2017-04-15 16:46:49 +0300

    CallGame

---
+++ Assets/Scripts/CallGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CallGame : MonoBehaviour {
+    public GameObject childNumber;
+    public GameType game;
+
+    private void OnMouseDown()
+    {
+        GameManager.Instance.StartMiniGame(game);
+    }
+   
+}
