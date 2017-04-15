0c8abbdb0c82a2097eaed9681aff5005ee85802c
isolomonik <irina.solomonik2@gmail.com>
2017-04-15 22:50:17 +0300

    KitchenDoor animation

---
+++ Assets/Scripts/AnimatedTouch.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedTouch : MonoBehaviour {
+    public Animator anim;
+    bool toOpen = true;
+    private void OnMouseDown()
+    {
+        Debug.Log(gameObject);
+    
+        anim.SetBool("toOpen", toOpen);
+        toOpen = !toOpen;
+    }
+}
